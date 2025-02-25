e5e0da2e5ef3e692182b65e48ec6bff6ac466688
phatch21 <phatch21@byu.edu>
2025-02-25 15:47:41 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Group4_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
