07a1a3e41227fa1cdd28b039ab128f8e34c4da20
emmahelquist <154380901+emmahelquist@users.noreply.github.com>
2025-02-28 15:41:33 -0700

    Added Delete and edit action and display views in the quadrant!

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,32 @@ namespace Mission08_Group4_6.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpGet]
+        public IActionResult Edit(Task updatedTask)
+        {
+            _context.Update(updatedTask);
+            _context.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Tasks
+                .Single(x => x.Id == id);
+            
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task recordToDelete)
+        {
+            _context.Tasks.Remove(recordToDelete);
+            _context.SaveChanges();
+        
+            return RedirectToAction("Index");
+        }
     }
 }
