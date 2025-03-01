fec8e55836af379be334c2dc0ba56d47b26388b1
phatch21 <phatch21@byu.edu>
2025-02-28 22:57:00 -0700

    deleted

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -114,7 +114,6 @@ namespace Mission08_Group4_6.Controllers
             return RedirectToAction("Index");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
