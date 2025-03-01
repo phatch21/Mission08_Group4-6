b146a57c36a9e831b04ca8be4e5ab52653413caf
phatch21 <phatch21@byu.edu>
2025-02-28 23:19:25 -0700

    two error

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -138,9 +138,5 @@ namespace Mission08_Group4_6.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
